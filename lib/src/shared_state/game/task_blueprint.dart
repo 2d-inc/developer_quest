23295417ef5e3054e764611cd0a2e9ab84990549
Filip Hracek <filip.hracek@gmail.com>
2019-03-05 15:35:36 -0800

    Add a simple Task tree with requirements
    This introduces the idea of `Prerequisite`, which is either another `TaskBlueprint`, or a combination of those (e.g. `AnyOf`, `AllOf`).

--- lib/src/shared_state/game/task_blueprint.dart
+++ lib/src/shared_state/game/task_blueprint.dart
@@ -1,18 +1,34 @@
 import 'package:dev_rpg/src/shared_state/game/skill.dart';
+import 'package:dev_rpg/src/shared_state/game/task_prerequisite.dart';
 import 'package:meta/meta.dart';
 
 /// A blueprint of a task.
 ///
 /// This class is immutable. Put runtime state into [Task].
 @immutable
-class TaskBlueprint {
+class TaskBlueprint implements Prerequisite {
   final String name;
 
   final Map<Skill, double> difficulty;
 
+  /// The tasks (and their combinations) that the player must have completed
+  /// before this task is available.
+  final Prerequisite requirements;
+
   final int xpReward;
+
+  const TaskBlueprint(
+    this.name,
+    this.difficulty, {
+    @required this.requirements,
+    this.xpReward = 100,
+  })  : assert(name != null),
+        assert(difficulty != null),
+        assert(requirements != null);
 
   List<Skill> get skillsNeeded => difficulty.keys.toList(growable: false);
 
-  const TaskBlueprint(this.name, this.difficulty, {this.xpReward = 100});
+  /// The task is satisfied if, and only if, it has already been done.
+  @override
+  bool isSatisfiedIn(Iterable<Prerequisite> done) => done.contains(this);
 }
