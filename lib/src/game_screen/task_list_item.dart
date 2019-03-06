974f46348fc19c67cd40850a3c363c6990ac3497
Filip Hracek <filip.hracek@gmail.com>
2019-03-05 19:31:08 -0800

    Limit rebuilds on each “physics” update
    Instead of rebuilding all of the TaskListItem, rebuild only the parts that actually can change.
    
    Hopefully this is not premature optimization. It’s good practice to only rebuild leaf nodes anyway.

--- lib/src/game_screen/task_list_item.dart
+++ lib/src/game_screen/task_list_item.dart
@@ -7,62 +7,64 @@ import 'package:dev_rpg/src/shared_state/provider.dart';
 import 'package:flutter/material.dart';
 
 class TaskListItem extends StatelessWidget {
-  TaskListItem({Key key}) : super(key: key);
+  final Task task;
+
+  TaskListItem({@required this.task, Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Provide<Task>(
-      builder: (context, child, task) => Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Card(
-              child: InkWell(
-                onTap: () => showModalBottomSheet<Set<Npc>>(
-                      context: context,
-                      builder: (context) => TeamPickerModal(task),
-                    ).then((npcs) => _onAssigned(task, npcs)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: <Widget>[
-                          Text(task.blueprint.name,
-                              style: TextStyle(fontSize: 14)),
-                          Expanded(
-                            child: Wrap(
-                              alignment: WrapAlignment.end,
-                              children: task.blueprint.skillsNeeded
-                                  .map((Skill skill) => SkillBadge(skill))
-                                  .toList(),
-                            ),
-                          )
-                        ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Card(
+        child: InkWell(
+          onTap: () => showModalBottomSheet<Set<Npc>>(
+                context: context,
+                builder: (context) => TeamPickerModal(task),
+              ).then((npcs) => _onAssigned(task, npcs)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(task.blueprint.name, style: TextStyle(fontSize: 14)),
+                    Expanded(
+                      child: Wrap(
+                        alignment: WrapAlignment.end,
+                        children: task.blueprint.skillsNeeded
+                            .map((Skill skill) => SkillBadge(skill))
+                            .toList(),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child:
-                          LinearProgressIndicator(value: task.percentComplete),
-                    ),
-                    task.assignedTeam == null
-                        ? SizedBox()
-                        : Container(
-                            height: 100.0,
-                            color: Colors.deepOrange,
-                            child: InkWell(
-                              onTap: () => task.boost += 2.5,
-                              child: Text('Team Pic Goes Here... assigned to: '
-                                  '${task.assignedTeam}. Tap to boost.'),
-                            ),
-                          ),
+                    )
                   ],
                 ),
               ),
-            ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Provide<Task>(
+                  builder: (context, child, snapshot) =>
+                      LinearProgressIndicator(value: snapshot.percentComplete),
+                ),
+              ),
+              Provide<Task>(builder: (context, child, snapshot) {
+                if (snapshot.assignedTeam == null) return SizedBox();
+                return Container(
+                  height: 100.0,
+                  color: Colors.deepOrange,
+                  child: InkWell(
+                    onTap: () => snapshot.boost += 2.5,
+                    child: Text('Team Pic Goes Here... assigned to: '
+                        '${snapshot.assignedTeam}. Tap to boost.'),
+                  ),
+                );
+              }),
+            ],
           ),
+        ),
+      ),
     );
   }
 
