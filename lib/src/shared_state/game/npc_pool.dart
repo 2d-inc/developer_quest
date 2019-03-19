35d7cc7247a6fd5a3fb7fb969ecd3122f5f62c34
Luigi Rosso <luigi.rosso@gmail.com>
2019-03-18 17:05:01 -0700

    Allow hiring and make bugs get assigned that the team can actually work on.

--- lib/src/shared_state/game/npc_pool.dart
+++ lib/src/shared_state/game/npc_pool.dart
@@ -7,14 +7,20 @@ import 'package:dev_rpg/src/shared_state/game/src/child_aspect.dart';
 class NpcPool extends AspectContainer<Npc> with ChildAspect {
   NpcPool() {
     addAspects([
-      Npc("The Refactorer", {Skill.coding: 1}),
+      Npc("The Refactorer", {Skill.coding: 1}, true),
       Npc("The Architect", {Skill.coding: 3, Skill.coordination: 1}),
-      Npc("TPM", {Skill.coordination: 3}),
+      Npc("TPM", {Skill.coordination: 3}, true),
       Npc("Avant Garde Designer", {Skill.engineering: 1}),
-      Npc("Leonardo", {Skill.ux: 2}),
+      Npc("Leonardo", {Skill.ux: 2}, true),
       Npc("Michelangelo", {Skill.engineering: 2}),
     ]);
   }
+
+  // get the set of available skills available with the player's hired team
+  Set<Skill> get availableSkills => children
+      .expand<Skill>((npc) =>
+          npc.isHired ? npc.prowess.keys : const Iterable<Skill>.empty())
+      .toSet();
 
   // Mark this Aspect dirty when any child is dirty.
   @override
--- lib/src/shared_state/game/skill.dart
+++ lib/src/shared_state/game/skill.dart
@@ -3,13 +3,17 @@ import 'dart:math';
 enum Skill { coding, engineering, ux, coordination }
 
 // get a random set of skills and difficutly values
-Map<Skill, double> randomDifficulty(Random randomizer,
+Map<Skill, double> randomDifficulty(
+    Random randomizer, Set<Skill> availableSkills,
     {double maxDifficulty = 100}) {
   Map<Skill, double> difficulty = {};
 
+  List<Skill> availableSkillList = availableSkills.toList();
+  
   // Add one or two skills.
   do {
-    var skill = Skill.values[randomizer.nextInt(Skill.values.length)];
+    var skill =
+        availableSkillList[randomizer.nextInt(availableSkillList.length)];
     difficulty[skill] = randomizer.nextDouble() * maxDifficulty;
   } while (difficulty.length < 2 && randomizer.nextBool());
 
