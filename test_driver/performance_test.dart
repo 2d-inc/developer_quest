12887d328e1b40ec44c1bc1ba1af5a3146c01edb
Filip Hracek <filip.hracek@gmail.com>
2019-04-03 09:08:56 -0700

    Remove package:test
    Instead of using package:test for the performance driver test, just use regular old main(). This is not really a test, there was no reasonable `expect()`.

--- test_driver/performance_test.dart
+++ test_driver/performance_test.dart
@@ -2,72 +2,18 @@ import 'dart:io';
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:t_stats/t_stats.dart';
-import 'package:test/test.dart';
 
-void main() {
-  group('walkthrough', () {
-    FlutterDriver driver;
+Future<void> main() async {
+  FlutterDriver driver;
 
-    // Connect to the Flutter driver before running any tests
-    setUpAll(() async {
-      driver = await FlutterDriver.connect();
-    });
-
-    // Close the connection to the driver after the tests have completed
-    tearDownAll(() async {
-      if (driver != null) {
-        await driver.close();
-      }
-    });
-
-    test('start and complete two tasks', () async {
-      var health = await driver.checkHealth();
-      expect(health.status, HealthStatus.ok);
-
-      await driver.tap(_startGameFinder);
-      await driver.waitForAbsent(_startGameFinder);
-      await driver.tap(find.text("Tasks"));
-
-      // Give the UI time to settle down before starting the trace.
-      await Future<void>.delayed(const Duration(milliseconds: 300));
-
-      await driver.startTracing();
-
-      await _completeTask(driver, 'Prototype');
-      await _completeTask(driver, 'Basic Backend');
-
-      var timeline = await driver.stopTracingAndDownloadTimeline();
-
-      var now = DateTime.now();
-      var name = 'walkthrough-${now.toIso8601String()}';
-      var filename = name.replaceAll(':', '-');
-
-      var summary = TimelineSummary.summarize(timeline);
-      await summary.writeSummaryToFile(filename, pretty: true);
-      await summary.writeTimelineToFile(filename);
-
-      var stat = Statistic.from(
-        summary.summaryJson['frame_build_times'] as List<int>,
-        name: name,
-      );
-      var tsv =
-          File('test_driver/perf_stats.tsv').openWrite(mode: FileMode.append);
-      // Add general build time statistics.
-      tsv.write(stat.toTSV());
-      // Add additional useful stats from the TimelineSummary.
-      tsv.write('\t');
-      tsv.write(summary.computePercentileFrameBuildTimeMillis(90.0));
-      tsv.write('\t');
-      tsv.write(summary.computePercentileFrameBuildTimeMillis(99.0));
-      tsv.write('\t');
-      tsv.write(summary.computeWorstFrameBuildTimeMillis());
-      tsv.write('\t');
-      tsv.write(summary.computeMissedFrameBuildBudgetCount());
-      tsv.writeln();
-      await tsv.close();
-      print(stat);
-    }, timeout: const Timeout(Duration(minutes: 5)));
-  });
+  try {
+    driver = await FlutterDriver.connect();
+    await _run(driver);
+  } finally {
+    if (driver != null) {
+      await driver.close();
+    }
+  }
 }
 
 final _addTaskButtonFinder = find.byValueKey('add_task');
@@ -97,4 +43,53 @@ Future _completeTask(FlutterDriver driver, String taskName) async {
   var shipIt = find.text('Ship it!!');
   await driver.waitFor(shipIt);
   await driver.tap(shipIt);
+}
+
+Future<void> _run(FlutterDriver driver) async {
+  var health = await driver.checkHealth();
+  if (health.status != HealthStatus.ok) {
+    throw StateError('FlutterDriver health: $health');
+  }
+
+  await driver.tap(_startGameFinder);
+  await driver.waitForAbsent(_startGameFinder);
+  await driver.tap(find.text("Tasks"));
+
+  // Give the UI time to settle down before starting the trace.
+  await Future<void>.delayed(const Duration(milliseconds: 300));
+
+  await driver.startTracing();
+
+  await _completeTask(driver, 'Prototype');
+  await _completeTask(driver, 'Basic Backend');
+
+  var timeline = await driver.stopTracingAndDownloadTimeline();
+
+  var now = DateTime.now();
+  var name = 'walkthrough-${now.toIso8601String()}';
+  var filename = name.replaceAll(':', '-');
+
+  var summary = TimelineSummary.summarize(timeline);
+  await summary.writeSummaryToFile(filename, pretty: true);
+  await summary.writeTimelineToFile(filename);
+
+  var stat = Statistic.from(
+    summary.summaryJson['frame_build_times'] as List<int>,
+    name: name,
+  );
+  var tsv = File('test_driver/perf_stats.tsv').openWrite(mode: FileMode.append);
+  // Add general build time statistics.
+  tsv.write(stat.toTSV());
+  // Add additional useful stats from the TimelineSummary.
+  tsv.write('\t');
+  tsv.write(summary.computePercentileFrameBuildTimeMillis(90.0));
+  tsv.write('\t');
+  tsv.write(summary.computePercentileFrameBuildTimeMillis(99.0));
+  tsv.write('\t');
+  tsv.write(summary.computeWorstFrameBuildTimeMillis());
+  tsv.write('\t');
+  tsv.write(summary.computeMissedFrameBuildBudgetCount());
+  tsv.writeln();
+  await tsv.close();
+  print(stat);
 }
