07f5782e8478b0d43c1877f1d9a694408348a3e6
Filip Hracek <filip.hracek@gmail.com>
2019-04-11 09:49:23 -0700

    Improve plots

--- test_driver/generate-graphs.R
+++ test_driver/generate-graphs.R
@@ -6,19 +6,29 @@ ids <- aggregate(durations$id, by=list(durations$id), FUN=head)[1]$Group.1
 # Get just the date and time from the id.
 labels <- substr(ids, 18, 33)
 
+# Setting plot margins.
+old.par <- par(no.readonly = TRUE)
+
 # Build durations
-pdf("test_driver/builds.pdf")
-boxplot(build ~ id, durations, notch = TRUE, boxwex = 0.8, xaxt = "n")
-axis(1, at = 1:length(ids), labels = labels, las = 2, cex.axis=0.5)
+pdf("test_driver/builds.pdf", width = 10, height = 10)
+par(mar = c(15,6,4,2)+0.1, las = 2)
+boxplot(build ~ description, durations, notch = TRUE, boxwex = 0.8)
+#axis(1, at = 1:length(ids), labels = labels, las = 2, cex.axis=0.5)
 title("Build times")
 dev.off()
-pdf("test_driver/rasterizations.pdf")
-boxplot(rasterizer ~ id, durations, notch = TRUE, boxwex = 0.8, xaxt = "n")
-axis(1, at = 1:length(ids), labels = labels, las = 2, cex.axis=0.5)
+
+pdf("test_driver/rasterizations.pdf", width = 10, height = 10)
+par(mar = c(15,6,4,2)+0.1, las = 2)
+boxplot(rasterizer ~ description, durations, notch = TRUE, boxwex = 0.8, las = 2)
+#axis(1, at = 1:length(ids), labels = labels, las = 2, cex.axis=0.5)
 title("Rasterizer times")
 dev.off()
-pdf("test_driver/frame_requests.pdf")
-boxplot(frameRequest ~ id, durations, notch = TRUE, boxwex = 0.8, xaxt = "n")
-axis(1, at = 1:length(ids), labels = labels, las = 2, cex.axis=0.5)
+
+pdf("test_driver/frame_requests.pdf", width = 10, height = 10)
+par(mar = c(15,6,4,2)+0.1, las = 2)
+boxplot(frameRequest ~ description, durations, notch = TRUE, boxwex = 0.8, las = 2)
+#axis(1, at = 1:length(ids), labels = labels, las = 2, cex.axis=0.5)
 title("Frame request pending times")
 dev.off()
+
+par(old.par)
