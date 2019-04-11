f64d2a6ca2972cf38f2842d105f0850426846332
Luigi Rosso <luigi.rosso@gmail.com>
2019-04-10 17:16:10 -0700

    Theming the app bar.

---
+++ lib/src/widgets/app_bar/stat_separator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// A widget to place between [StatBadge] widgets to show clear
+/// separation between them.
+class StatSeparator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 1,
+      color: const Color.fromRGBO(38, 38, 47, 1.0),
+    );
+  }
+}
