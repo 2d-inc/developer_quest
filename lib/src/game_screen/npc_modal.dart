5365ca1af09a69c39d03837f8723211e98b117eb
Luigi Rosso <luigi.rosso@gmail.com>
2019-04-15 13:25:43 -0700

    Fixing formatting.

--- lib/src/game_screen/npc_modal.dart
+++ lib/src/game_screen/npc_modal.dart
@@ -127,7 +127,8 @@ class NpcModal extends StatelessWidget {
                                               const EdgeInsets.only(top: 5.0),
                                           child: ProwessProgress(
                                               color: skillColor[skill],
-											  borderRadius: BorderRadius.circular(3.5),
+                                              borderRadius:
+                                                  BorderRadius.circular(3.5),
                                               progress:
                                                   npc.prowess[skill] / 100),
                                         )
