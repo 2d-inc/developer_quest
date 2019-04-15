46f48998f4245bd67dff791bff6abb6864f8d146
Filip Hracek <filip.hracek@gmail.com>
2019-04-15 16:22:23 -0700

    Add another chart output of perf-testing

--- test_driver/generate-graphs.R
+++ test_driver/generate-graphs.R
@@ -1,5 +1,6 @@
 # Load data from disk.
 durations <- read.csv("test_driver/durations.tsv", sep = "\t")
+durations <- durations[durations$description == "baseline" | durations$description == "statvalue-always-notifies" ,]
 
 # Get just the IDs as a vector.
 ids <- aggregate(durations$id, by=list(durations$id), FUN=head)[1]$Group.1
@@ -8,6 +9,21 @@ labels <- substr(ids, 18, 33)
 
 # Setting plot margins.
 old.par <- par(no.readonly = TRUE)
+
+# Percentiles
+build90 <- aggregate(build ~ description, durations, function(x) quantile(x, c(0.5,0.90)))
+build95 <- aggregate(build ~ description, durations, function(x) quantile(x, c(0.5,0.95)))
+build99 <- aggregate(build ~ description, durations, function(x) quantile(x, c(0.5,0.99)))
+len <- length(build95$description)
+labels <- gsub("-", "\n", build95$description)
+
+# Jank chart
+pdf("test_driver/builds_bad.pdf", width = 16, height = 9)
+par(mar = c(15,6,4,2)+0.1, las = 2)
+x <- barplot(t(build95$build), xlim = c(0, max(build99$build)), density = len:1 * 2, angle = len:1 * 90 + 45, names.arg = labels, horiz = TRUE, beside = TRUE)
+#arrows(x, build90$build, x, build99$build, length=0.05, angle=90, code=3)
+title("Build times worst case")
+dev.off()
 
 # Build durations
 pdf("test_driver/builds.pdf", width = 10, height = 10)
