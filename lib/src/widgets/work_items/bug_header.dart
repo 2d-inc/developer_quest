9d5bfd7b713397dc54c384155fc30750b4ba3caa
Brian Egan <brian@brianegan.com>
2019-04-24 15:16:57 +0200

    Fix formatting of bug_header.dart

--- lib/src/widgets/work_items/bug_header.dart
+++ lib/src/widgets/work_items/bug_header.dart
@@ -7,7 +7,9 @@ import 'package:flutter/material.dart';
 /// Indicator for bug list items. Shows skills necessary to fix the bug.
 class BugHeader extends StatelessWidget {
   final Bug bug;
+
   const BugHeader(this.bug);
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -15,7 +17,7 @@ class BugHeader extends StatelessWidget {
         const Icon(Icons.bug_report, color: bugColor),
         Expanded(child: Container()),
         for (Skill skill in bug.skillsNeeded) SkillDot(skill)
-      ]
+      ],
     );
   }
 }
