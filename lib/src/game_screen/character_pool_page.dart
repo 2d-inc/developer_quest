cdbd9eae60aff7cf1e7198f9b7f01ee14186626f
Luigi Rosso <luigi.rosso@gmail.com>
2019-04-30 20:36:57 -0700

    Improving layout for wide screens. Issue #96.

--- lib/src/game_screen/character_pool_page.dart
+++ lib/src/game_screen/character_pool_page.dart
@@ -10,30 +10,35 @@ import 'package:provider/provider.dart';
 
 /// Displays a list of [Character]s that are available to the player.
 class CharacterPoolPage extends StatelessWidget {
+  final int numColumns;
+  const CharacterPoolPage({this.numColumns = 2});
+
   @override
   Widget build(BuildContext context) {
     var characterPool = Provider.of<CharacterPool>(context);
-    return Stack(
-      children: [
-        GridView.builder(
-          padding: const EdgeInsets.only(left: 15, right: 15, bottom: 128),
-          itemCount: characterPool.children.length,
-          gridDelegate: _gridStructure,
-          itemBuilder: (context, index) =>
-              ChangeNotifierProvider<Character>.value(
-                notifier: characterPool.children[index],
-                key: ValueKey(characterPool.children[index]),
-                child: CharacterListItem(),
-              ),
-        ),
-        _fadeOverlay,
-      ],
+    return Container(
+      child: Stack(
+        children: [
+          GridView.builder(
+            padding: const EdgeInsets.only(left: 15, right: 15, bottom: 128),
+            itemCount: characterPool.children.length,
+            gridDelegate: _gridStructure,
+            itemBuilder: (context, index) =>
+                ChangeNotifierProvider<Character>.value(
+                  notifier: characterPool.children[index],
+                  key: ValueKey(characterPool.children[index]),
+                  child: CharacterListItem(),
+                ),
+          ),
+          _fadeOverlay,
+        ],
+      ),
     );
   }
 
   SliverGridDelegate get _gridStructure =>
-      const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+      SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: numColumns,
           mainAxisSpacing: 0,
           crossAxisSpacing: 15,
           childAspectRatio: 0.65);
