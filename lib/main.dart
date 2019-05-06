fc19d9ff4deddbcd6db25c92b6fbcb224b874340
Luigi Rosso <luigi.rosso@gmail.com>
2019-05-05 20:30:35 -0700

    Disable landscape on phones.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:ui' as ui;
 
 import 'package:dev_rpg/src/about_screen.dart';
 import 'package:dev_rpg/src/code_chomper/code_chomper.dart';
 import 'package:dev_rpg/src/game_screen.dart';
 import 'package:dev_rpg/src/shared_state/game/world.dart';
 import 'package:dev_rpg/src/shared_state/user.dart';
+import 'package:dev_rpg/src/style.dart';
 import 'package:dev_rpg/src/style_sphinx/axis_questions.dart';
 import 'package:dev_rpg/src/style_sphinx/flex_questions.dart';
 import 'package:dev_rpg/src/style_sphinx/kittens.dart';
@@ -20,6 +22,16 @@ void main() {
   // Don't prune the Flare cache, keep loaded Flare files warm and ready
   // to be re-displayed.
   FlareCache.doesPrune = false;
+
+  var window = ui.window;
+  var width = window.physicalSize.width / window.devicePixelRatio;
+  if (width < blockLandscapeThreshold) {
+    // Disallow rotating to landscape.
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
 
   runApp(MyApp());
 }
